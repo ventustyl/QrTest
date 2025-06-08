98a32034a94963a90aafd2715867c4c69d66cc31
Eric Venturino <34771970+ventustyl@users.noreply.github.com>
2025-06-08 19:25:46 +0200

    Register services and add DI test

--- QrTest/Services/ApplicationService.cs
+++ QrTest/Services/ApplicationService.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using QrTest.ViewModels;
+
 namespace QrTest.Services
 {
-    internal class ApplicationService
+    internal class ApplicationService : IApplicationService
     {
+        public Task ProcessQrAsync(string payload)
+        {
+            // Application-specific processing would occur here
+            return Task.CompletedTask;
+        }
     }
 }
