0d7bd6ee48be4710914f9f88b1ec296176efb504
Eric Venturino <34771970+ventustyl@users.noreply.github.com>
2025-06-08 19:27:41 +0200

    Add AppShell and register ScanPage route

--- QrTest/App.xaml.cs
+++ QrTest/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "QrTest" };
+            return new Window(new AppShell()) { Title = "QrTest" };
         }
     }
 }
